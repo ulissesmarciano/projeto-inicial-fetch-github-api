9c46a67be4aa041641deccc511bf31e69270abe0
Ulisses <ulissesmkt2104@gmail.com>
2024-10-14 17:58:09 -0300

    Adicionando a renderização de todo o componente.

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -19,14 +19,52 @@ const screen = {
             <h2>Repositórios</h2>
             <ul>${repositorieItens}</ul>
             </div>`
-        }
+        } 
+
+        // let eventItens = ''
+        // user.events.forEach(event => eventItens += `<li ><a>${event.repo.name}</a> - ${event.payload.commits?.[0].message}</li>`)
+        
+        // if (user.events.type === 'PushEvent' || 'CreateEvent'){
+        //     this.userProfile.innerHTML += `<div class="events section">
+        //     <h2>Eventos</h2>
+        //     <ul>${eventItens}</ul>
+        //     </div>`
+        // }
+
+        let eventListFiltered = user.events.filter(
+            event => event.type === "PushEvent" || event.type === "CreateEvent"
+        )
+        let eventList = eventListFiltered.splice(0, 10)
+        let events  = ''
+
+        eventList.forEach(event => {
+            let eventRepoName = ''
+            let eventCommit = ''
+            if (event.type === "PushEvent"){
+                eventRepoName = event.repo.name
+                eventCommit = event.payload.commits[0].message
+
+            } else if (event.type === "CreateEvent"){
+                eventRepoName = event.repo.name
+                eventCommit = "Não possui commits"
+            } else return
+
+            events += `<li><a href="https://github.com/${eventRepoName}" target="_blank">${eventRepoName}</a> - ${eventCommit}</li>`
+        })
+        
+        this.userProfile.innerHTML += `<div class="events">
+                                        <h2>Eventos</h2>
+                                        <ul>${events}</ul>
+                                      </div>`
+        
     },
+
 
     
 
     renderNotFound(){
         this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
-    }
+    },
 }
 
 export { screen }
